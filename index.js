f0cea9bfce069d29fbe172fccd4cbef424498e3a
MilkMother <118103942+MilkMother@users.noreply.github.com>
2023-06-05 13:09:16 +0200

    design doc update, blog post finish

--- index.js
+++ index.js
@@ -1,16 +1,13 @@
-//Find all headers in the list
 const accordionItemHeaders = document.querySelectorAll(
   ".accordion-item-header"
 );
 
-//Iterate trough each header from the list and add
 accordionItemHeaders.forEach((accordionItemHeader) => {
   accordionItemHeader.addEventListener("click", (event) => {
-    //When I click on the row
     const currentlyActiveAccordionItemHeader = document.querySelector(
       ".accordion-item-header.active"
     );
-    //If I had expanded header before, close it
+
     if (
       currentlyActiveAccordionItemHeader &&
       currentlyActiveAccordionItemHeader !== accordionItemHeader
@@ -19,7 +16,6 @@ accordionItemHeaders.forEach((accordionItemHeader) => {
       currentlyActiveAccordionItemHeader.nextElementSibling.style.maxHeight = 0;
     }
 
-    //Current header that I clicked on, expand it
     accordionItemHeader.classList.toggle("active");
     const accordionItemBody = accordionItemHeader.nextElementSibling;
     if (accordionItemHeader.classList.contains("active")) {
