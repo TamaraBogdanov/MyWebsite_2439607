cc1a727e2cf6111a4b477f5fd6773605b0147d7d
MilkMother <118103942+MilkMother@users.noreply.github.com>
2023-06-26 15:13:00 +0200

    js changes and final commit

--- index.js
+++ index.js
@@ -25,3 +25,13 @@ accordionItemHeaders.forEach((accordionItemHeader) => {
 });
 
 const removeDuplicates = (arr) => [...new Set(arr)];
+
+const toTop = document.querySelector(".toTop");
+
+window.addEventListener("scroll", () => {
+  if (window.pageYOffset > 100) {
+    toTop.classList.add("active");
+  } else {
+    toTop.classList.remove("active");
+  }
+});
